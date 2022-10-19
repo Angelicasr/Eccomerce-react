1156db5156d4f54a1b5397c16eb9d05b91ddc01d
angelica sierra <m.angelicasr25@gmail.com>
2022-10-19 13:19:53 -0300

    commit

--- src/components/ItemDetailContainer/index.jsx
+++ src/components/ItemDetailContainer/index.jsx
@@ -5,7 +5,7 @@ import {getFirestore, doc, getDoc} from 'firebase/firestore';
 
 
 
-export const ItemDetailContainer = () => {
+const ItemDetailContainer = () => {
     const [data, setData] = useState({});
     const {detalleId} = useParams();
 
